93c881288dfcd1ccfaf9d4d888dfe76a8de09715
Sanjeev  Mishra <56467938+sanjeevmishra391@users.noreply.github.com>
2022-01-27 22:22:10 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// js
+// const heading = document.createElement('h1');
+// heading.innerText = 'Hey';
+// const root = document.getElementById('root')
+// root.appendChild(heading);
+
+// jsx
+// const name = 'Jack';
+
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName
+// }
+
+// const user = null;
+
+// function greeting(user) {
+//   if(user) {
+//     return <h1>Hey {formatName(user)}</h1>
+//   } else {
+//     return <h1>Hey someone</h1>
+//   }
+// }
+
+// // "Good Morning Jack Shelby"
+// // const heading = <h1></h1>;
+// const imgURL = 'https://images.unsplash.com/photo-1643149005853-8f0e1b6a6cbf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1000&q=60'
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />  
+  ,document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
