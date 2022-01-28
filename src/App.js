33f5431e907c00e0c729e455e10f3ae2a4e2d262
Sanjeev  Mishra <56467938+sanjeevmishra391@users.noreply.github.com>
2022-01-28 20:03:58 +0530

    added searchbox component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
 import './App.css';
-
+import Searchbox from './components/searchbox/Searchbox';
 import CardList from './components/cardlist/CardList';
+import './App.css'
+
+// constructor -- render -- componentDidMount (state change - queue render function) -- render
 
 // class components
 class App extends Component {
@@ -19,19 +22,35 @@ class App extends Component {
     .then(users => this.setState({usersData: users}))
   }
 
+  handleChange = (e) => {
+    this.setState({searchText: e.target.value}, () => {
+        console.log(this.state.searchText);
+    })
+  }
+
   render() {
+
+    const {usersData, searchText} = this.state;
+
+    // const usersData = this.state.usersData;
+    // const searchText = this.state.searchText;
+
+    const filterUser = usersData.filter((user) => user.name.toLowerCase().includes(searchText.toLowerCase()))
+
+    // usersData: [list of 10 users],
+    // searchText: 'g'
+    // filterUser: [list of filtered users having name that contains 'g']
+
+    // ['Jack', 'John', 'George', 'Thomas']
+    // 'o' -- ['John', 'George', 'Thomas']
+    // Every string contains empty string.
+
     return (
       <div className='app'>
 
-        <input type='search' placeholder='Search users' 
-          onChange={(e) => { 
-            this.setState({searchText: e.target.value}, () => {
-                console.log(this.state.searchText);
-              })
-          }} 
-        />
+        <Searchbox placeholder={'Search users'} handleChange={this.handleChange} />
 
-        <CardList usersData={this.state.usersData} />
+        <CardList usersData={filterUser} />
         
       </div>
     );
